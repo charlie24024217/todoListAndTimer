99bb8433ae25a6cb69db361d794b536ee70e7bb8
Bagel <abc24024217@gmail.com>
2023-03-10 18:30:41 +0800

    CSS ALL DAY

---
+++ app.js
@@ -0,0 +1,37 @@
+let list = document.querySelector("#list");
+let listStatus = "fold";
+list.addEventListener("click", () => {
+  switchDisplay();
+});
+
+function switchDisplay() {
+  if (window.innerWidth > 1200) {
+    if (listStatus == "open") {
+      $("#list").css("position", "fixed");
+      $("#list").css("right", -400 + "px");
+      listStatus = "fold";
+    } else {
+      $("#list").css("position", "relative");
+      $("#list").css("right", 0);
+      listStatus = "open";
+    }
+  } else if (window.innerWidth > 600 && window.innerWidth < 1200) {
+    if (listStatus == "open") {
+      $("#list").css("position", "fixed");
+      $("#list").css("right", -400 + "px");
+      listStatus = "fold";
+    } else {
+      $("#list").css("position", "absolute");
+      $("#list").css("right", 50);
+      listStatus = "open";
+    }
+  } else {
+    if (listStatus == "open") {
+      $("#list").css("bottom", -500 + "px");
+      listStatus = "fold";
+    } else {
+      $("#list").css("bottom", 0 + "%");
+      listStatus = "open";
+    }
+  }
+}
